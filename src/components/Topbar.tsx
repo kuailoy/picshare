5032173c08dde14b32eade26cdf8b4f9a89919fe
kuailoy <kuailoy@gmail.com>
2026-04-19 22:37:29 +0300

    fix: UI fix

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,7 @@
 export default function Topbar() {
   return (
     <header className="sticky top-0 z-40 h-16 w-full border-b border-gray-200 bg-white">
-      <div className="mx-auto flex h-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-full max-w-[1960px] items-center justify-between px-4">
         <div className="text-xl font-semibold text-gray-900">PicShare</div>
       </div>
     </header>
