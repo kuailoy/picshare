ecf0a5c59a0c8e22d1349eee9aa9c329f9dd9d03
kuailoy <kuailoy@gmail.com>
2026-04-19 22:40:41 +0300

    add: type safty

--- src/app/api/uploads/sign/route.ts
+++ src/app/api/uploads/sign/route.ts
@@ -1,11 +1,12 @@
 // app/api/uploads/sign/route.ts
 import { NextResponse } from 'next/server'
 import { generateUploadSignature } from '@/server/storage/cloudinary'
+import type { NextRequest } from 'next/server'
 
 
 // Param folder is not required. If the server can read the userID from the session,
 // it can be used to create user-specific folders.
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const body = await request.json().catch(() => ({}))
   const folder = typeof body.folder === 'string' ? body.folder : undefined
 
